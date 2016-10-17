17c974be5b3a3e001111a9e6d3b07f7bf388e04a
Tony He <tony.he@hirede.com>
2016-10-17 18:08:16 +0800

    门列表修改scroller样式

--- mobile/src/components/product/door/Doors.vue
+++ mobile/src/components/product/door/Doors.vue
@@ -7,6 +7,7 @@
       <slot>
         <li class="list-item" v-for="item in listDoors">
           <card v-link="{name: 'productDoorDetail', params: {id: item.id}}">
+            <img slot="header" v-bind:src="item.images && item.images[0] && item.images[0].url" v-if="item.images"/>
             <div slot="content" class="card-padding">
               <p style="color:#999;font-size:12px;">{{item.createdAt}}</p>
               <p style="font-size:14px;line-height:1.2;">{{item.title}}</p>
